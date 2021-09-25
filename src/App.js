e96bd4c380f40e70d15d4586c64fd02f3b037337
Salman Nazeer <salmanselu2002@gmail.com>
2021-09-25 11:12:20 +0530

    Add some codes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import {useEffect} from 'react';
 import './App.css';
 
+
+
+
 function App() {
+  let [searchText, setSearchTextCount] = useState("");
   return (
     <div className="App">
-      
+      <h1>Welcome to dict App</h1>
+      <div>
+        <input type="text" name="searchWord"></input>
+        <button>search</button>
+        <p className="word"></p>
+      </div>
     </div>
   );
 }
