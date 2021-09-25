3f267ed711ced37654f8249f6086ad3e914e41d7
Salman Nazeer <salmanselu2002@gmail.com>
2021-09-25 12:19:42 +0530

    Add a conditional

--- src/App.js
+++ src/App.js
@@ -11,12 +11,16 @@ function App() {
     setSearchTextCount(event.target.value)
     console.log(event.target.value)
   }
+    let onSubmit = () =>  {
+      console.log("button click");
+    }
+
   return (
     <div className="App">
       <h1 style={{color: searchText===""?"red":"black"}}>Welcome to dict App {searchText}</h1>
       <div>
         <input onChange={onChangeText} type="text" name="searchWord"></input>
-        <button>search</button>
+        <button onClick={onSubmit}>search</button>
         {searchText === "" ?
          <p>Please type something here</p>
          :
