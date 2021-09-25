a0daa75a7c7ed542ba931b96db868987e03ee280
Salman Nazeer <salmanselu2002@gmail.com>
2021-09-25 11:28:27 +0530

    How to take input

--- src/App.js
+++ src/App.js
@@ -7,11 +7,15 @@ import './App.css';
 
 function App() {
   let [searchText, setSearchTextCount] = useState("");
+  let onChangeText = (event) => {
+    setSearchTextCount(event.target.value)
+    console.log(event.target.value)
+  }
   return (
     <div className="App">
-      <h1>Welcome to dict App</h1>
+      <h1>Welcome to dict App {searchText}</h1>
       <div>
-        <input type="text" name="searchWord"></input>
+        <input onChange={onChangeText} type="text" name="searchWord"></input>
         <button>search</button>
         <p className="word"></p>
       </div>
